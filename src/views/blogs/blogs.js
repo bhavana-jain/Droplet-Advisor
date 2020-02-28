088aab0476aff2c14f1b5a462382234a00fbb2cf
bhavana-jain <bhavanachajjed@gmail.com>
2020-02-28 23:08:28 +0530

    updated UI for blogs

--- src/views/blogs/blogs.js
+++ src/views/blogs/blogs.js
@@ -13,10 +13,10 @@ const newdata = data.map((data) => {
 	let truncateDescription = description.substring(0, 150) + '...  ';
 	let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	let blogDate = new Date(data.createdOn).toString();
-	console.log(months[new Date(data.createdOn).getMonth() - 1]);
 	blogDate = `${months[new Date(data.createdOn).getMonth()]} ` + `${new Date(data.createdOn).getDate()}` + ` , ` + `${new Date(data.createdOn).getFullYear()}`;
 	blogLength++;
 	console.log('blog length', blogLength)
+
 	return (
 		<div key={data.id} className="blog-list">
 			<img src={`/images/${data.image}.jpg`}></img>
@@ -47,23 +47,29 @@ let filterData = () => data.map(function (data) {
 		const BlogName = data.comp;
 		const BlogNameUrl = "/blog/" + BlogName;
 		const blogComp = data.compName;
+		let description = data.description;
+		let truncateDescription = description.substring(0, 150) + '...  ';
 		let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 		let blogDate = new Date(data.createdOn).toString();
 		blogDate = `${months[new Date(data.createdOn).getMonth()]} ` + `${new Date(data.createdOn).getDate()}` + ` , ` + `${new Date(data.createdOn).getFullYear()}`;
 		return (
 
 			<div key={data.id} className="blog-list">
-				<img src={`images/${data.image}.jpg`}></img>
+				<img src={`/images/${data.image}.jpg`}></img>
 				<div className="blog-description-wrap">
 					<div className="blog-description">
-						<div><h3>{data.title}</h3></div>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...
-		 			<NavLink to={{ pathname: `${BlogNameUrl}`, state: `${blogComp}` }}>
-								Learn More
-		    </NavLink>
-						</p>
+						<div className="blog-title-outer">
+							<h3>{data.title}</h3>
+							<p className="blog-otherInfo"> {blogDate} </p>
+						</div>
+						<div className="blog-nav-link">
+							<p>{truncateDescription}
+								<NavLink to={{ pathname: `${BlogNameUrl}`, state: `${blogComp}` }}>
+									Learn More
+            </NavLink></p>
+						</div>
 					</div>
-					<p className="blog-otherInfo">By {data.author} | {data.category} | {blogDate} </p>
+
 				</div>
 
 			</div>
@@ -79,7 +85,9 @@ let archiveData = () => data.map(function (data) {
 	blogMonth = months[new Date(data.createdOn).getMonth()];
 	blogYear = new Date(data.createdOn).getFullYear();
 	archiveMonth = months[new Date(archiveYear).getMonth()];
-	archiveYr = new Date(archiveYear).getFullYear()
+	archiveYr = new Date(archiveYear).getFullYear();
+	let description = data.description;
+	let truncateDescription = description.substring(0, 150) + '...  ';
 	console.log(months[new Date(data.createdOn).getMonth()], new Date(data.createdOn).getFullYear())
 	console.log("archive", months[new Date(archiveYear).getMonth()], new Date(archiveYear).getFullYear())
 	if (blogMonth == archiveMonth && blogYear == archiveYr) {
@@ -90,19 +98,22 @@ let archiveData = () => data.map(function (data) {
 		return (
 
 			<div key={data.id} className="blog-list">
-				<img src={`images/${data.image}.jpg`}></img>
+				<img src={`/images/${data.image}.jpg`}></img>
 				<div className="blog-description-wrap">
 					<div className="blog-description">
-						<div><h3>{data.title}</h3></div>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam...
-		 			<NavLink to={{ pathname: `${BlogNameUrl}`, state: `${blogComp}` }}>
-								Learn More
-		    </NavLink>
-						</p>
+						<div className="blog-title-outer">
+							<h3>{data.title}</h3>
+							<p className="blog-otherInfo"> {blogDate} </p>
+						</div>
+						<div className="blog-nav-link">
+							<p>{truncateDescription}
+								<NavLink to={{ pathname: `${BlogNameUrl}`, state: `${blogComp}` }}>
+									Learn More
+            </NavLink></p>
+						</div>
 					</div>
-					<p className="blog-otherInfo">By {data.author} | {data.category} | {blogDate} </p>
-				</div>
 
+				</div>
 			</div>
 
 		)
@@ -112,9 +123,21 @@ let archiveData = () => data.map(function (data) {
 export default class Blogs extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			blogs: newdata,
+			currentPage: 1,
+			todosPerPage: 3
+		}
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(event) {
+		this.setState({
+			currentPage: Number(event.target.id)
+		});
 	}
 
 	render() {
+
 		searchVal = this.props.filter;
 		archiveYear = this.props.archive;
 		return (
